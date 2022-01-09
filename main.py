f4725b99e6d9321cf4b1eaee17d21f38d3bd5665
verTuego <82771385+ver-tuego@users.noreply.github.com>
2022-01-09 12:17:35 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import vk_api
+import time
+from threading import Thread
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+try:
+    from config import access_token
+except ImportError:
+    exit('Необходимо указать токен в "access_token" из config.py')
+
+if access_token == '':
+    exit('Необходимо указать токен в "access_token" из config.py')
+
+vk_session = vk_api.VkApi(token=access_token)
+api = vk_session.get_api()
+longpoll = VkLongPoll(vk_session)
+
+def getBanned(vk_session):
+    result = vk_session.method("account.getBanned")
+    if result['count'] == 0:
+        return 1
+    else:
+        return result['items']
+
+getTime = time.time() - 1
+listBanned = getBanned(vk_session)
+
+def Handler(event):
+
+    global getTime, listBanned
+    user_id = event.user_id
+
+    if (time.time() - getTime) > 0:
+        listBanned = getBanned(vk_session)
+        getTime = time.time() + 30
+
+    if event.peer_id > 2000000000 and listBanned != 1:
+        if user_id in listBanned:
+            vk_session.method("messages.delete", {"peer_id": event.peer_id,
+                                                  "message_ids": event.message_id})
+
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW:
+        newEvent = Thread(target=Handler, args=(event,))
+        newEvent.start()
