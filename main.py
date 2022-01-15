af1c9431e52f14f74fcdf692241a806ae1b71a3b
verTuego <82771385+ver-tuego@users.noreply.github.com>
2022-01-15 20:49:09 +0300

    добавлена возможность игнорировать пользователя
    указывать в config.py из ignoreBan

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import vk_api
+import re
 import time
 from threading import Thread
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 try:
-    from config import access_token
+    from config import access_token, ignoreBan
 except ImportError:
-    exit('Необходимо указать токен в "access_token" из config.py')
+    exit('Необходимо проверить все нужные элементы в config.py')
 
 if access_token == '':
     exit('Необходимо указать токен в "access_token" из config.py')
@@ -30,12 +31,14 @@ def Handler(event):
     global getTime, listBanned
     user_id = event.user_id
 
+    print(f'#LOGS | {time.strftime("%m.%d %H:%M:%S")} | {user_id} | ', re.sub(r"\s", " ", event.text))
+
     if (time.time() - getTime) > 0:
         listBanned = getBanned(vk_session)
         getTime = time.time() + 30
 
     if event.peer_id > 2000000000 and listBanned != 1:
-        if user_id in listBanned:
+        if user_id in listBanned and user_id not in ignoreBan:
             vk_session.method("messages.delete", {"peer_id": event.peer_id,
                                                   "message_ids": event.message_id})
 
@@ -43,3 +46,4 @@ for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
         newEvent = Thread(target=Handler, args=(event,))
         newEvent.start()
+
